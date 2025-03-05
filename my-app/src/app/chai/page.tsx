7aaec9e1c741c022fff3fa57b986feef0ed91f63
GauravVaishnav24 <gauravvaishnav76@gmail.com>
2025-03-05 23:53:35 +0530

    first next app

---
+++ my-app/src/app/chai/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: "Chai page",
+};
+
+function page() {
+  return (
+    <div>
+      <main className='bg-amber-800 flex items-center justify-center min-h-screen'>
+        <h1 className='text-4xl lading-[130%] text-amber-200'>Chai page</h1>
+      </main>
+    </div>
+  )
+}
+
+export default page
+
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-amber-300 flex justify-center items-center min-h-screen">
+      <h1 className="text-2xl leading-[130%]">Home Page</h1>
+    </div>
+  );
+}
